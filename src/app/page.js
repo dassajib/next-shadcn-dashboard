2c38a37143732a07adee5a22f898b57dd2276e6c
dassajib <sajibdas.sd@gmail.com>
2025-06-05 01:58:15 +0600

    Chart's and date field's are added in home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,27 +1,29 @@
+import CardList from "@/components/card/CardList";
 import AppAreaChart from "@/components/charts/AppAreaChart";
 import AppBarChart from "@/components/charts/AppBarChart";
 import AppPieChart from "@/components/charts/AppPieChart";
+import ToDoList from "@/components/ToDoList";
 
 export default function Home() {
   return (
     <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-4 p-4">
-      <div className="bg-primary-foreground p-4 rounded-lg">
+      <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-2 2xl:col-span-2">
         <AppBarChart />
       </div>
-      <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">
-        Test
+      <div className="bg-primary-foreground p-4 rounded-lg">
+        <CardList title="Popular Content" />
       </div>
       <div className="bg-primary-foreground p-4 rounded-lg">
-        Test
+        <AppPieChart />
       </div>
       <div className="bg-primary-foreground p-4 rounded-lg">
-        Test
+        <CardList title="Latest Transactions" />
       </div>
       <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">
         <AppAreaChart />
       </div>
       <div className="bg-primary-foreground p-4 rounded-lg">
-        <AppPieChart />
+        <ToDoList />
       </div>
     </div>
   );
