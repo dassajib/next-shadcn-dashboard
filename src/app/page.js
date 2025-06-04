d951ff8ba0e7dc6ecd56d09cf1587032f389b3bd
dassajib <sajibdas.sd@gmail.com>
2025-06-04 17:36:43 +0600

    chart's added

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,28 @@
+import AppAreaChart from "@/components/charts/AppAreaChart";
+import AppBarChart from "@/components/charts/AppBarChart";
+import AppPieChart from "@/components/charts/AppPieChart";
+
 export default function Home() {
   return (
-   <div>Hello NextJS</div>
+    <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-4 p-4">
+      <div className="bg-primary-foreground p-4 rounded-lg">
+        <AppBarChart />
+      </div>
+      <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">
+        Test
+      </div>
+      <div className="bg-primary-foreground p-4 rounded-lg">
+        Test
+      </div>
+      <div className="bg-primary-foreground p-4 rounded-lg">
+        Test
+      </div>
+      <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">
+        <AppAreaChart />
+      </div>
+      <div className="bg-primary-foreground p-4 rounded-lg">
+        <AppPieChart />
+      </div>
+    </div>
   );
 }
